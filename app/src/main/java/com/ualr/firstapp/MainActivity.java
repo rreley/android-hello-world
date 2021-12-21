0d47cfc3f5b47189cd0952ae0ee60a5276e38d6b
Robert Eley <r.eley21@gmail.com>
2021-12-20 21:01:45 -0600

    Initial commit

---
+++ app/src/main/java/com/ualr/firstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.ualr.firstapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ualr.firstapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View rootView = mBinding.getRoot();
+        setContentView(rootView);
+    }
+
+    // TODO 06. Avoid updating the text label (userMsgTV) when the text field (userInputET) is empty
+    public void showTextMessage(View view) {
+        String text = mBinding.userInputET.getText().toString();
+        if (!text.isEmpty()) {
+            mBinding.userMsgTV.setText(text);
+        }
+    }
+
+    // TODO 07. Create a new method called cleanTextField to delete the text inside the text field
+    public void cleanTextField(View view) {
+        mBinding.userInputET.setText(null);
+    }
+}
